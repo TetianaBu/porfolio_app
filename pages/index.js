0613900369f1cbb50b142ff7eb0e0e4e6175b343
TetianaBu <tetianataker@gmail.com>
2021-09-23 21:33:05 +0200

    del utilsStyles line

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
-import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (

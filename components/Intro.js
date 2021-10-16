dab7f98fd80612ebaa4430f8d54f6736d48962dc
TetianaBu <tetianataker@gmail.com>
2021-10-16 15:05:16 +0200

    des changes

--- components/Intro.js
+++ components/Intro.js
@@ -9,7 +9,7 @@ export default function Intro() {
     <div className={styles.wide}>
     <div className={styles.intro}>
     <p className={styles.upheading}>Hi, there! My name is</p>
-    <h1>Tetiana Buchok and I’m Front-End Developer</h1>
+    <h1>Tetiana Buchok and I’m <span className={styles.animated}>Front-End Developer</span></h1>
     <p className={styles.subheading}>I’m focused on creating apps using React and Next.js. Currently I’m looking for Junior/Trainee Front-End Developer role in Cracow or remote.</p>
     <Link href="/resume.pdf"><a className={styles.resumeBtn}>Grab Resume</a></Link>
     <Link href="/game"><a className={styles.gameBtn}>Play Game</a></Link>
